ca1995659b8d0d1842edc9c69d18890b315a2530
Quadeatschool <quadehafen@byui.edu>
2025-05-03 23:05:07 -0600

    p0

---
+++ intro.py
@@ -0,0 +1,15 @@
+# %%
+import math
+
+print(math.sqrt(16))
+
+# %%
+# This is a new cell
+for i in range(5):
+    print(i)
+# %%
+from lets_plot import *
+from palmerpenguins import load_penguins
+
+LetsPlot.setup_html()
+# %%
